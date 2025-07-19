a0cffcd247d0409e6262fd44fb060a529373246c
Lu1zReis <eluiz8204@gmail.com>
2025-07-19 14:02:26 -0400

    Adicionando novas funcoes

--- AnalisadorLexico/main.py
+++ AnalisadorLexico/main.py
@@ -3,14 +3,15 @@ TOKENS = [
     "FUNC", "INT", "REAL", "STRING", "LISTA", "SE", "PODESER", 
     "SENAO", "PARA", "ENQUANTO", "RETORNO", "JUROU", "CERTIN", "CONST",
     "OP_LOGICO", "OP_NUMERICO", "OP_RELACIONAL",
-    "VAR", "DEL", "STRING_VAL", "LISTA_VAL", "INT_VAL", "REAL_VAL", "CLASSE"
+    "IDENT", "DEL", "STRING_VAL", "LISTA_VAL", "INT_VAL", "REAL_VAL", "CLASSE"
 ]
 
 delimitadores = [",", ";", "(", ")", "{", "}"]
 operadores_logic = ["&", "|", "!"]
-operadores_aritm = ["+", "-", "*", "/", "%", "**"]
+operadores_aritm = ["+", "-", "*", "/", "%"]
 operadores_relac = ["=", ">", "<"]
 
+# variavel global
 resultado = []
 
 # regras de cada token
@@ -25,7 +26,20 @@ def isIdent(buffer):
 
 def isClass(buffer):
     size = len(buffer)
-    return size > 2 and size <= 24 and buffer[0] == '/'
+    return size > 1 and size <= 24 and buffer[0] == buffer[0].upper() and buffer[0].isalpha()
+
+def isFloat(buffer):
+    # "10.2"
+    tevePontuacao = False
+    for digit in buffer:
+        if not digit.isdigit() and digit != '.':
+            return False
+        elif digit == '.':
+            tevePontuacao = True
+    
+    if tevePontuacao:
+        return True
+    return False
 
 def isDelim(buffer):
     return buffer in delimitadores
@@ -35,6 +49,11 @@ def isOpLogic(buffer):
 
 def isOpRelac(buffer):
     return buffer in operadores_relac
+
+def isOpAritm(buffer):
+    return buffer in operadores_aritm
+
+#############################################
 
 # funcoes auxiliares
 def isEnd(caractere):
@@ -48,27 +67,27 @@ def display():
     for token, lexema in resultado:
         print(f"<{token}, {lexema}>")
 
+#############################################
+
 def main():
-    codigo = "/Classe_01() {teste > teste2;}"
+    codigo = "/Classe_01() {1.0 + 10.2;}"
     buffer = ""
 
     for i, carac in enumerate(codigo):
-        carac_unico = carac in delimitadores or carac in operadores_logic or carac in operadores_relac
-        if isEnd(carac) or carac_unico:
-            # lendo caracteres que precisam ser adicionados no buffer para funcionar
-            entrarParaClassificar = True
-            if carac == '/':
-                buffer += carac
-                entrarParaClassificar = False
-
+        carac_unico_especial = carac in delimitadores or carac in operadores_logic or carac in operadores_relac or carac in operadores_aritm
+        if isEnd(carac) or carac_unico_especial:
             # tentar classificar o buffer antes de limpar
-            if buffer and entrarParaClassificar:
+            if buffer:
                 if isClass(buffer):
                     add(buffer, "CLASSE")
                 elif isIdent(buffer):
-                    add(buffer, "IDENT")  # token padrão
+                    add(buffer, "IDENT")  
+                elif isFloat(buffer):
+                    add(buffer, "REAL")
                 else:
                     add("Erro", "ERRO")
+
+                # sempre ira limpar
                 buffer = ""
 
             # tratando quando é só um caractere
@@ -78,6 +97,9 @@ def main():
                 add(carac, "OP_LOGIC")
             elif isOpRelac(carac):
                 add(carac, "OP_RELAC")
+            elif isOpAritm(carac):
+                add(carac, "OP_ARITM")
+                
         else:
             buffer += carac
 
