6f2c1bceaac8d883d46ad44949f792ea97a67698
Lu1zReis <eluiz8204@gmail.com>
2025-09-07 21:15:46 -0400

    Atualizando mensagens no parser

--- AnalisadorSintatico/main.py
+++ AnalisadorSintatico/main.py
@@ -36,11 +36,13 @@ def carregar_tokens(caminho_arquivo):
             if linha.startswith("<") and "," in linha:
                 tipo = linha.split(",")[0].strip("<>").strip()
                 lexema = linha.split(",")[1].strip(" >")
-                
+
+                # ignorando o cabeçalho
                 if tipo == "TOKEN":  
-                    continue  # ignora cabeçalho
+                    continue  
                 elif tipo == "EOF":
-                    tokens.append(("$", "$"))  # adiciona símbolo de fim de entrada
+                    # adicionando símbolo de fim de entrada
+                    tokens.append(("$", "$"))  
                 else:
                     tokens.append((tipo, lexema))
     return tokens
@@ -59,22 +61,30 @@ def parser_ll1(tokens, parse_table, start_symbol="E"):
 
         if top == "ε":
             continue
-        elif top == current:  # terminal casa
-            print(f"✔ Reconhecido: {current}")
+
+        # terminal casa
+        elif top == current:  
+            print(f"Reconhecido: {current}")
             cursor += 1
-        elif top in parse_table:  # não terminal
+
+        # não terminal
+        elif top in parse_table:
             if current in parse_table[top]:
                 production = parse_table[top][current]
-                print(f"{top} → {' '.join(production)}")
+                print(f"{top} -> {' '.join(production)}")
                 for symbol in reversed(production):
                     stack.append(symbol)
             else:
-                raise SyntaxError(f"Erro: não há produção para {top} com lookahead {current}")
+                expected = list(parse_table[top].keys())
+                raise SyntaxError(
+                    f"Erro de sintaxe: para '{top}', esperado {expected}, mas encontrou '{current}'"
+                )
+
         else:
             raise SyntaxError(f"Erro: esperado {top}, encontrado {current}")
 
         if top == "$" and current == "$":
-            print("✅ Análise concluída com sucesso!")
+            print("Análise concluída com sucesso!")
             return True
 
     return False
@@ -82,4 +92,7 @@ def parser_ll1(tokens, parse_table, start_symbol="E"):
 
 if __name__ == "__main__":
     tokens = carregar_tokens("tokens_saida.txt")  # arquivo com os <TOKEN, LEXEMA>
-    parser_ll1(tokens, parse_table)
+    if (parser_ll1(tokens, parse_table)):
+        print("A gramática pertence à linguagem.")
+    else:
+        print("A gramática não pertence à linguagem.")
